b2272b9bb6ce07287e4e901be5efb61db1f59bc0
조해창 <outofchng@gmail.com>
2023-05-22 19:53:02 +0900

    fix: jest 테스트를 위한 eslint role 수정

--- truffle-config.js
+++ truffle-config.js
@@ -1,29 +1,32 @@
 module.exports = {
-    // Solidity 컴파일러 설정
-    compilers: {
-        solc: {
-            version: "0.8.4", // Solidity 버전을 지정합니다.
-        },
+  // Solidity 컴파일러 설정
+  compilers: {
+    solc: {
+      version: '0.8.4', // Solidity 버전을 지정합니다.
     },
-    // 네트워크 설정
-    networks: {
-        node1: { // Node1 설정
-            host: "168.188.129.232",
-            port: 80,
-            network_id: "*",
-        },
-        node2: { // Node2 설정
-            host: "168.188.129.231",
-            port: 80,
-            network_id: "*",
-        },
-        node3: { // Node3 설정
-            host: "168.188.129.206",
-            port: 80,
-            network_id: "*",
-        },
-        // 더 많은 네트워크를 추가할 수 있습니다.
+  },
+  // 네트워크 설정
+  networks: {
+    node1: {
+      // Node1 설정
+      host: '168.188.129.232',
+      port: 80,
+      network_id: '*',
     },
-    // 컨트랙트 소스 코드 위치 설정
-    contracts_directory: './src/contracts'
+    node2: {
+      // Node2 설정
+      host: '168.188.129.231',
+      port: 80,
+      network_id: '*',
+    },
+    node3: {
+      // Node3 설정
+      host: '168.188.129.206',
+      port: 80,
+      network_id: '*',
+    },
+    // 더 많은 네트워크를 추가할 수 있습니다.
+  },
+  // 컨트랙트 소스 코드 위치 설정
+  contracts_directory: './src/contracts',
 };
