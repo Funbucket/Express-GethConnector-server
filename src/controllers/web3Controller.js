6cedca2943d927bf15e41c3b322348d243b6600c
Dae-Hee-Ryu <eogml0716@gmail.com>
2023-05-23 01:54:49 +0900

    [UPDATE] NFTPhoto Contract 관련 파일을 사용하기 위해서 코드 수정
    Comment :

--- src/controllers/web3Controller.js
+++ src/controllers/web3Controller.js
@@ -2,13 +2,9 @@
 const Web3 = require('web3');
 // 상수를 정의한 파일을 가져옵니다.
 const constants = require('../constants');
-// 파일 시스템 관련 작업을 위한 Node.js 내장 모듈을 가져옵니다.
-const fs = require('fs');
-// Solidity 컴파일러를 가져옵니다.
-const solc = require('solc');
 
 // Truffle contract를 가져옵니다.
-const contract = require("@truffle/contract");
+const TruffleContract = require("@truffle/contract");
 // NFTPhoto 컨트랙트 아티팩트를 가져옵니다.
 const contractJSON = require("../build/contracts/NFTPhoto.json");
 
@@ -20,81 +16,65 @@ exports.createWeb3Instance = () => {
 
 // 스마트 계약의 인스턴스를 가져오는 함수를 정의합니다.
 exports.getContractInstance = (web3Instance) => {
-  let NFTPhoto = contract(contractJSON);
+  const NFTPhoto = TruffleContract(contractJSON);
   NFTPhoto.setProvider(web3Instance.currentProvider);
   return NFTPhoto;
 };
 
-async function getCoinBalanceFromBlockchain(web3Instance, address) {
-  try {
-    const NFTPhoto = exports.getContractInstance(web3Instance);
-    const contractInstance = await NFTPhoto.deployed();
-    // Now use contractInstance to call your contract methods
-  } catch (error) {
-    console.error(`Failed to load contract: ${error}`);
-  }
-}
-
 // 트랜잭션을 보내는 함수를 정의합니다.
 exports.sendTransaction = async (
-  contract,
-  address,
-  gasLimit,
-  method,
-  ...methodArgs
+    contractInstance,
+    address,
+    gasLimit,
+    method,
+    ...methodArgs
 ) => {
-  // eslint-disable-next-line no-useless-catch
   try {
-    // 계약의 메서드를 호출하고 트랜잭션을 보냅니다.
-    await contract.methods[method](...methodArgs).send({
+    await contractInstance.methods[method](...methodArgs).send({
       from: address,
       gas: gasLimit,
     });
   } catch (error) {
-    // 에러가 발생하면 에러를 던집니다.
     throw error;
   }
 };
 
 // 사진 NFT를 구매하는 함수를 정의합니다.
-exports.buyPhotoNFT = async (contract, buyerAddress, tokenId, price) => {
-  // 'buyPhotoNFT' 메서드를 호출하는 트랜잭션을 보냅니다.
+exports.buyPhotoNFT = async (contractInstance, buyerAddress, tokenId, price) => {
   return exports.sendTransaction(
-    contract,
-    buyerAddress,
-    constants.GAS_LIMIT,
-    'buyPhotoNFT',
-    tokenId,
-    price
+      contractInstance,
+      buyerAddress,
+      constants.GAS_LIMIT,
+      'buyPhotoNFT',
+      tokenId,
+      price
   );
 };
 
 // 사진 NFT의 가격을 업데이트하는 함수를 정의합니다.
 exports.updatePhotoNFTPrice = async (
-  contract,
-  ownerAddress,
-  tokenId,
-  newPrice
-) => {
-  // 'updatePhotoNFTPrice' 메서드를 호출하는 트랜잭션을 보냅니다.
-  return exports.sendTransaction(
-    contract,
+    contractInstance,
     ownerAddress,
-    constants.GAS_LIMIT,
-    'updatePhotoNFTPrice',
     tokenId,
     newPrice
+) => {
+  return exports.sendTransaction(
+      contractInstance,
+      ownerAddress,
+      constants.GAS_LIMIT,
+      'updatePhotoNFTPrice',
+      tokenId,
+      newPrice
   );
 };
 
 // 사진 NFT를 삭제하는 함수를 정의합니다.
-exports.deletePhotoNFT = async (contract, ownerAddress, tokenId) => {
-  // 'deletePhotoNFT' 메서드를 호출하는 트랜잭션을 보냅니다.
+exports.deletePhotoNFT = async (contractInstance, ownerAddress, tokenId) => {
   return exports.sendTransaction(
-    contract,
-    ownerAddress,
-    constants.GAS_LIMIT,
-    'deletePhotoNFT',
-    tokenId
+      contractInstance,
+      ownerAddress,
+      constants.GAS_LIMIT,
+      'deletePhotoNFT',
+      tokenId
   );
 };
